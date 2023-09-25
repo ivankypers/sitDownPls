ed380cb464d690256ae811b9771a36eaddfdd2ca
CyberGnida <ivan.mol.10@mail.ru>
2023-09-25 23:42:50 +0300

    Special block with slider + fixes

--- src/js/swiper-main.js
+++ src/js/swiper-main.js
@@ -1,8 +1,45 @@
 const swiper = new Swiper('.hero__swiper', {
     direction: 'horizontal',
     slidesPerView: 1,
+    speed: 5500,
+    centeredSlides: true,
     loop: true,
+    cssMode: true,
     autoplay: {
-        delay: 5000
-    }
+        delay: 5000,
+    },
 });
+
+const swipeSpecial = new Swiper('.special__swiper', {
+    direction: 'horizontal',
+    slidesPerView: 1,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    spaceBetween: '100%',
+    speed: 1000,
+});
+
+const swipeSpecialBtnPrev = document.querySelector('.swiper-button-prev')
+const swipeSpecialBtnNext = document.querySelector('.swiper-button-next')
+
+swipeSpecialBtnPrev.addEventListener('click', () => {
+    if (swipeSpecialBtnPrev.classList.contains('swiper-button-disabled')) {
+        swipeSpecialBtnPrev.classList.remove('swiper-button__available')
+        swipeSpecialBtnNext.classList.add('swiper-button__available')
+    } else {
+        swipeSpecialBtnNext.classList.add('swiper-button__available')
+        swipeSpecialBtnPrev.classList.add('swiper-button__available')
+    }
+})
+
+swipeSpecialBtnNext.addEventListener('click', () => {
+    if (swipeSpecialBtnNext.classList.contains('swiper-button-disabled')) {
+        swipeSpecialBtnNext.classList.remove('swiper-button__available')
+        swipeSpecialBtnPrev.classList.add('swiper-button__available')
+    } else {
+        swipeSpecialBtnNext.classList.add('swiper-button__available')
+        swipeSpecialBtnPrev.classList.add('swiper-button__available')
+    }
+})
