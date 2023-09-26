74b870ddc7f11179ce59afb61d04df3d82f7a086
CyberGnida <ivan.mol.10@mail.ru>
2023-09-26 20:29:38 +0300

    added next sections. Created swiper button functions

--- src/js/swiper-main.js
+++ src/js/swiper-main.js
@@ -1,14 +1,35 @@
 const swiper = new Swiper('.hero__swiper', {
     direction: 'horizontal',
     slidesPerView: 1,
-    speed: 5500,
+    speed: 100,
     centeredSlides: true,
     loop: true,
     cssMode: true,
     autoplay: {
-        delay: 5000,
+        delay: 10000,
     },
 });
+
+
+function SwiperButtonsPrev() {
+    if (swipeSpecialBtnPrev.classList.contains('swiper-button-disabled')) {
+        swipeSpecialBtnPrev.classList.remove('swiper-button__available')
+        swipeSpecialBtnNext.classList.add('swiper-button__available')
+    } else {
+        swipeSpecialBtnNext.classList.add('swiper-button__available')
+        swipeSpecialBtnPrev.classList.add('swiper-button__available')
+    }
+}
+
+function swiperButtonsNext() {
+    if (swipeSpecialBtnNext.classList.contains('swiper-button-disabled')) {
+        swipeSpecialBtnNext.classList.remove('swiper-button__available')
+        swipeSpecialBtnPrev.classList.add('swiper-button__available')
+    } else {
+        swipeSpecialBtnNext.classList.add('swiper-button__available')
+        swipeSpecialBtnPrev.classList.add('swiper-button__available')
+    }
+}
 
 const swipeSpecial = new Swiper('.special__swiper', {
     direction: 'horizontal',
@@ -24,22 +45,8 @@ const swipeSpecial = new Swiper('.special__swiper', {
 const swipeSpecialBtnPrev = document.querySelector('.swiper-button-prev')
 const swipeSpecialBtnNext = document.querySelector('.swiper-button-next')
 
-swipeSpecialBtnPrev.addEventListener('click', () => {
-    if (swipeSpecialBtnPrev.classList.contains('swiper-button-disabled')) {
-        swipeSpecialBtnPrev.classList.remove('swiper-button__available')
-        swipeSpecialBtnNext.classList.add('swiper-button__available')
-    } else {
-        swipeSpecialBtnNext.classList.add('swiper-button__available')
-        swipeSpecialBtnPrev.classList.add('swiper-button__available')
-    }
-})
 
-swipeSpecialBtnNext.addEventListener('click', () => {
-    if (swipeSpecialBtnNext.classList.contains('swiper-button-disabled')) {
-        swipeSpecialBtnNext.classList.remove('swiper-button__available')
-        swipeSpecialBtnPrev.classList.add('swiper-button__available')
-    } else {
-        swipeSpecialBtnNext.classList.add('swiper-button__available')
-        swipeSpecialBtnPrev.classList.add('swiper-button__available')
-    }
-})
+
+swipeSpecialBtnPrev.addEventListener('click', SwiperButtonsPrev)
+
+swipeSpecialBtnNext.addEventListener('click', swiperButtonsNext)
