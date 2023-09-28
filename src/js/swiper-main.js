a4ceed03983861874a4b674409e6dccac28e3d7d
CyberGnida <ivan.mol.10@mail.ru>
2023-09-28 21:31:37 +0300

    blog swiper + feedback section

--- src/js/swiper-main.js
+++ src/js/swiper-main.js
@@ -11,6 +11,29 @@ const swiper = new Swiper('.hero__swiper', {
 });
 
 
+const swipeSpecial = new Swiper('.special__swiper', {
+    direction: 'horizontal',
+    slidesPerView: 1,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    spaceBetween: "100%",
+    speed: 1000,
+});
+
+
+const swipeBlog = new Swiper('.blog__swiper', {
+    direction: 'horizontal',
+    slidesPerView: 2,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    spaceBetween: 32,
+    speed: 1000,
+});
+
 function SwiperButtonsPrev() {
     if (swipeSpecialBtnPrev.classList.contains('swiper-button-disabled')) {
         swipeSpecialBtnPrev.classList.remove('swiper-button__available')
@@ -31,22 +54,17 @@ function swiperButtonsNext() {
     }
 }
 
-const swipeSpecial = new Swiper('.special__swiper', {
-    direction: 'horizontal',
-    slidesPerView: 1,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    spaceBetween: '100%',
-    speed: 1000,
-});
-
 const swipeSpecialBtnPrev = document.querySelector('.swiper-button-prev')
 const swipeSpecialBtnNext = document.querySelector('.swiper-button-next')
+
+const swipeBlogBtnPrev = document.querySelector('.blog__swiper-button-prev')
+const swipeBlogBtnNext = document.querySelector('.blog__swiper-button-next')
 
 
 
 swipeSpecialBtnPrev.addEventListener('click', SwiperButtonsPrev)
-
 swipeSpecialBtnNext.addEventListener('click', swiperButtonsNext)
+swipeBlogBtnPrev.addEventListener('click', SwiperButtonsPrev)
+swipeBlogBtnNext.addEventListener('click', swiperButtonsNext)
+
+
